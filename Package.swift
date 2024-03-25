29b3939e9c9e632dfa5b10bd7bc2ab83d22f666c
kbland2866 <163484955+kbland2866@users.noreply.github.com>
2024-03-25 15:25:48 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "NHN Cloud SDK",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         .library(
             name: "NHNCloudSDK",
